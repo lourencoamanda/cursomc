72a6306198024a80672835630b0b1e266d919605
Amanda Lourenco <amandalourenco67@gmail.com>
2020-04-28 20:05:34 -0300

    Endpoint/clientes/{id}disponivel

--- src/main/java/com/cursomc/CursomcApplication.java
+++ src/main/java/com/cursomc/CursomcApplication.java
@@ -86,9 +86,6 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll((Arrays.asList(e1,e2)));
 		
-		
-		
-
 	}
 
 }
